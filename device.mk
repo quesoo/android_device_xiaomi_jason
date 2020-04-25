ac00e0acbf8692dd81a83cf000445614d44d6ad7
jimmy <quesoo@gmail.com>
2020-04-25 14:13:14 +0200

    jason: allow community to edit camera props in build.prop

--- device.mk
+++ device.mk
@@ -83,7 +83,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.dualcam.lpm.enable=1 \
     persist.vendor.dualcam.defer.enable=1
 
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+PRODUCT_PROPERTY_OVERRIDES += \
     vendor.camera.aux.packageblacklist=com.tencent.mm \
     vendor.camera.aux.packagelist=com.android.camera,org.codeaurora.snapcam,com.google.android.GoogleCamera \
     vendor.camera.hal1.packagelist=com.whatsapp,com.intsig.camscanner,com.instagram.android
